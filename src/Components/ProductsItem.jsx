94328264330dea44d30607802a908eacba9c13af
MOHDmujahed1990 <109663959+MOHDmujahed1990@users.noreply.github.com>
2022-12-09 00:25:05 +0530

    xxxx

--- src/Components/ProductsItem.jsx
+++ src/Components/ProductsItem.jsx
@@ -1,27 +1,32 @@
-import { Card, CardContent, Grid } from "@mui/material";
+import { Card, CardContent, Grid,Rating } from "@mui/material";
 import React from "react";
+import Carousel from 'react-material-ui-carousel'
+
 
 export const ProductsItem=({item})=>{
     return(
         <React.Fragment>
             <Grid item xs={6} >
                 <Card align="center">
-                    <CardContent sx={{height:820,bgcolor:"#FAFAD2"}}>
-                          <h4> id: {item.id}</h4>
-                          <h4> Title: {item.title}</h4>
+                    <CardContent sx={{height:850,bgcolor:"#FAFAD2"}}>
+                          {/* <h4> id: {item.id}</h4> */}
+                          <h4> Brand: {item.title}</h4>
                           <img src={item.thumbnail} height="200px" width={"400px"} alt="" /> <br />
                           <h4> Description: {item.description}</h4>
                           <h4> Price: {item.price} $</h4>
                           <h4> Discount: {item.discountPercentage} %</h4>
-                          <h4> Rating: {item.rating}</h4>
+                          {/* <h4> Rating: {item.rating}</h4> */}
+                          <h4><Rating value={item.rating}/></h4>
                           <h4> Stock: {item.stock}</h4>
                           <h4> Brand: {item.brand}</h4>
                           <h4> Category: {item.category}</h4>
-                          <img src={item.images[0]} height="100px" alt="" />
-                          <img src={item.images[1]} height="100px" alt="" />
-                          <img src={item.images[2]} height="100px" alt="" />
-                          <img src={item.images[3]} height="100px" alt="" />
-                          <img src={item.images[4]} height="100px" alt="" />
+                          <Carousel  navButtonsAlwaysVisible interval={"5000"} stopAutoPlayOnHover >
+                          <img src={item.images[0]} height="150px" alt="" />
+                          <img src={item.images[1]} height="150px" alt="" />
+                          <img src={item.images[2]} height="150px" alt="" />
+                          <img src={item.images[3]} height="150px" alt="" />
+                          <img src={item.images[4]} height="150px" alt="" />
+                          </Carousel>
                     </CardContent>
                 </Card>
             </Grid>
